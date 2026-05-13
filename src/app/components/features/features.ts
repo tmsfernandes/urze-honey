0f556622df339aba1fa910de8c8a1d94406f8fa9
Tiagho Fernandes <tmsfernandes@hotmail.com>
2026-05-13 23:15:04 +0100

    Build URZE homepage layout and responsive structure

--- src/app/components/features/features.ts
+++ src/app/components/features/features.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-features',
-  standalone: true,
   imports: [],
   templateUrl: './features.html',
   styleUrl: './features.scss',
