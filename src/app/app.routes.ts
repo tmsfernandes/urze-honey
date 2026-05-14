ff40b02d23cd2a555e8cb53af3ab3546532978a8
Tiagho Fernandes <tmsfernandes@hotmail.com>
2026-05-14 19:15:25 +0100

    Remove unused product detail route for deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { ProductDetail } from './pages/product-detail/product-detail';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
     pathMatch: 'full'
-  },
-  {
-    path: 'products/:id',
-    component: ProductDetail
   }
 ];
